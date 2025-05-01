434c2a47ba773700052d45c1c6f6a2d8fa7a9c17
SmanB13 <emmanuel@berthod.net>
2025-05-01 17:38:29 +0200

    ✅ Correction V3 : ajout d'une entité sensor pour activer interface HA (icône + bouton Configurer)

--- custom_components/biopool_newgen/sensor.py
+++ custom_components/biopool_newgen/sensor.py
@@ -1,1 +1,18 @@
-# Placeholder sensor
+
+from homeassistant.components.sensor import SensorEntity
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback) -> None:
+    async_add_entities([BioPoolStatusSensor()])
+
+class BioPoolStatusSensor(SensorEntity):
+    _attr_name = "BioPool Status"
+    _attr_unique_id = "biopool_status"
+    _attr_icon = "mdi:water-pump"
+    _attr_native_unit_of_measurement = None
+    _attr_state_class = None
+
+    def __init__(self):
+        self._attr_native_value = "OK"
