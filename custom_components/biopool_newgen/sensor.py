8617d995cb0e9044084e19f10303439608a6f37a
SmanB13 <emmanuel@berthod.net>
2025-05-02 16:47:15 +0200

    🧪 CLEAN9 alpha9 : entité sans ID ni device_info, style SmartIrrigation

--- custom_components/biopool_newgen/sensor.py
+++ custom_components/biopool_newgen/sensor.py
@@ -4,12 +4,11 @@ from homeassistant.core import HomeAssistant
 from .const import DOMAIN
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities):
-    async_add_entities([PiscineBioPoolStatus(entry)], True)
+    async_add_entities([PiscineStatusSensor()])
 
-class PiscineBioPoolStatus(SensorEntity):
-    def __init__(self, entry: ConfigEntry):
-        self._attr_name = "État Piscine BioPool"
-        self._attr_unique_id = f"{DOMAIN}_main_status_v8"
-        self._attr_native_value = "Fonctionnel"
+class PiscineStatusSensor(SensorEntity):
+    def __init__(self):
+        self._attr_name = "Piscine Bio - Statut"
+        self._attr_native_value = "Actif"
         self._attr_should_poll = False
         self._attr_icon = "mdi:pool"
